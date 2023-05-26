554d04280246e634511e53e52f2efd7bf2822f2c
Diego Aguilar <d.aguilaro99@gmail.com>
2023-05-26 11:43:15 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -2,46 +2,32 @@ import { ConnectWallet } from "@thirdweb-dev/react";
 import "./styles/Home.css";
 
 export default function Home() {
+  const buttonStyle = {
+    backgroundColor: "#292999", // Change this to the color you want for the button
+    border: "none",
+    color: "white", // Change this to the color you want for the text
+    padding: "15px 32px", // Change this to adjust the padding around the text
+    textAlign: "center",
+    textDecoration: "none",
+    display: "inline-block",
+    fontSize: "16px", // Change this to adjust the size of the text
+    margin: "4px 2px",
+    cursor: "pointer",
+  };
+
   return (
     <div className="container">
       <main className="main">
         <h1 className="title">
-          Welcome to <a href="https://thirdweb.com/">thirdweb</a>!
+          Welcome to <a href="https://thirdweb.com/">ToL</a>!
         </h1>
 
         <p className="description">
-          Get started by configuring your desired network in{" "}
-          <code className="code">src/index.js</code>, then modify the{" "}
-          <code className="code">src/App.js</code> file!
+          Free mint to show your interest!
         </p>
 
         <div className="connect">
-          <ConnectWallet dropdownPosition={{ side: 'bottom', align: 'center'}} />
-        </div>
-
-        <div className="grid">
-          <a href="https://portal.thirdweb.com/" className="card">
-            <h2>Portal &rarr;</h2>
-            <p>
-              Guides, references and resources that will help you build with
-              thirdweb.
-            </p>
-          </a>
-
-          <a href="https://thirdweb.com/dashboard" className="card">
-            <h2>Dashboard &rarr;</h2>
-            <p>
-              Deploy, configure and manage your smart contracts from the
-              dashboard.
-            </p>
-          </a>
-
-          <a href="https://portal.thirdweb.com/templates" className="card">
-            <h2>Templates &rarr;</h2>
-            <p>
-              Discover and clone template projects showcasing thirdweb features.
-            </p>
-          </a>
+          <ConnectWallet dropdownPosition={{ side: 'bottom', align: 'center'}} style={buttonStyle} />
         </div>
       </main>
     </div>
